02894c8e1edfa23570da93fb90735b03dfd83915
git1 <2875012676@qq.com>
2022-01-06 23:05:57 +0800

    git试验3

--- MaShiBing01/src/test22/GitTest.java
+++ MaShiBing01/src/test22/GitTest.java
@@ -1,4 +1,8 @@
 package test22;
 
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+        System.out.println("hello,world!");
+    }
 }
